4656f97a636b8abb1bafde2f099bb5141087625f
LucasEduChaves <lucasedchaves090@gmail.com>
2023-10-06 12:30:35 -0300

    JS

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 import { Router } from './router.js'
 
 const router = new Router()
-router.add("/inferior", "/pages/inferior.html")
-router.add("/superior", "/pages/superior.html")
+router.add("/inferior", "./pages/inferior.html")
+router.add("/superior", "./pages/superior.html")
 router.add("/abdomen", "./pages/abdomen.html")
+router.add("/user", "./user/recover/user.html")
 router.add("/", "/pages/404.html")
 
 router.handle()
